a8e29d49b2395e46b3ffe5b30f83774e347e8c32
edgarucho <edgarv0211@gmail.com>
2026-08-06 18:07:01 -0600

    Update API_URL to new Google Apps Script URL

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // ============================================
 
 // La URL del web app de Apps Script. Termina en /exec
-const API_URL = 'https://script.google.com/macros/s/PEGA_AQUI_TU_URL/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzeaIY2Fjtk6ZFtJo8vDeFZtC2PseVaNyLdYrf--w4ZJE9btPMojK94v3V31Mt0LbiA/exec';
